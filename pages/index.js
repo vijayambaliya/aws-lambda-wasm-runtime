ecee3bbe76574ad9d3c9b9d729bb6795f27d7ba6
vijayambaliya <vijayambaliya89@gmail.com>
2023-06-12 16:11:44 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 
+
 export default function Home() {
   const [enableWasm, setEnableWasm] = useState(false);
   const [origImg, setOrigImg] = useState(null);
